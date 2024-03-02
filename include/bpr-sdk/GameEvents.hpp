3c020449ce476c24367ac8821078171547094097
Matty <54020396+matty-ross@users.noreply.github.com>
2024-03-02 19:34:04 +0100

    Added game events.

---
+++ include/bpr-sdk/GameEvents.hpp
@@ -0,0 +1,57 @@
+#pragma once
+
+
+#include <cstdint>
+
+
+namespace BPR
+{
+    bool GameEventQueue_AddGameEvent(void* gameEventQueue, const void* gameEvent, int32_t gameEventID, uint32_t gameEventSize)
+    {
+        bool result = false;
+        
+        __asm
+        {
+            push dword ptr [gameEventSize]
+            push dword ptr [gameEventID]
+            push dword ptr [gameEvent]
+            mov ecx, dword ptr [gameEventQueue]
+            
+            mov eax, 0x004E3F70
+            call eax
+
+            mov byte ptr [result], al
+        }
+
+        return result;
+    }
+
+    
+    struct SetupPlayerVehicleEvent
+    {
+        static constexpr int32_t k_GameEvent_ID = 0;
+
+        alignas(0x10) float Position[3];
+        alignas(0x10) float Direction[3];
+        uint64_t VehicleID;
+        uint64_t WheelID;
+    };
+
+    struct TeleportPlayerVehicleEvent
+    {
+        static constexpr int32_t k_GameEvent_ID = 1;
+
+        alignas(0x10) float Position[3];
+        alignas(0x10) float Direction[3];
+    };
+
+    struct ChangePlayerVehicleEvent
+    {
+        static constexpr int32_t k_GameEvent_ID = 2;
+
+        uint64_t VehicleID;
+        uint64_t WheelID;
+        bool ResetPlayerCamera;
+        bool KeepResetSection;
+    };
+}
