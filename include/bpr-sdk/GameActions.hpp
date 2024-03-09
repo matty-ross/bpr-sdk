39114c6b7ba4e3627808d56b0f0237ceceea46ef
Matty <54020396+matty-ross@users.noreply.github.com>
2024-03-09 20:16:52 +0100

    Added SetBoost action.

--- include/bpr-sdk/GameActions.hpp
+++ include/bpr-sdk/GameActions.hpp
@@ -41,4 +41,33 @@ namespace BPR
             return 0x4;
         }
     };
+
+    struct GameAction_SetBoost
+    {
+        int32_t ActiveRaceVehicleIndex;
+        struct
+        {
+            bool InfiniteBoost : 1;
+            bool BoostAmount : 1;
+            bool BoostSegments : 1;
+            bool BoostEarningEnabled : 1;
+            bool BoostMessagesEnabled : 1;
+            bool EmptyBoost : 1;
+        } Flags;
+        float BoostAmount;
+        int32_t BoostSegments;
+        bool InfiniteBoost;
+        bool BoostEarningEnabled;
+        bool BoostMessagesEnabled;
+
+        static constexpr int32_t GetID()
+        {
+            return 197;
+        }
+
+        static constexpr uint32_t GetSize()
+        {
+            return 0x14;
+        }
+    };
 }
